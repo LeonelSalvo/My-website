791d838f1c993220646e9c9d0ef73c670775a32e
Leo <73452456+LeonelSalvo@users.noreply.github.com>
2023-08-24 17:36:53 -0300

    Change cards exp

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,13 @@ import SocialBar from '@/components/socialBar'
 
 const Index = () => {
 	const gameDescription =
-		'Game developer hobbist since I have studied Videogame programming and design at Da Vinci First Multimedial School'
+		'Game developer hobbist in Unity and Godot since I have studied Video-game programming and design at Da Vinci First Multimedial School'
 
 	const frontDescription =
-		'Frontend engineer since 2019 React + TS + web3 from the very beginning. Some dApps: AMM | Swaps | NFT marketplaces | Lending | Liquid Staking | Unity WebGL Games '
+		'Frontend engineer since 2019 and Tech Lead React Typescript Web3. Some dApps: AMM | Swaps | NFT marketplaces | Lending | Liquid Staking | Unity WebGL Games '
 
 	const backDescription =
-		'Since 2020 backend programmer in parallel working with middleware | Nest.JS bots | solidity smart contracts'
+		'Since 2020 backend programmer working with middleware, Nest.JS bots and solidity smart contracts'
 
 	const artDescription =
 		'I know they are too cute to not notice but have you look these penguins!? Well, they are my creation using pyxel edit'
